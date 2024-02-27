9935f9c68003efc5e7a6eb3c49be897d1b35ed50
Jose M. Gomez <gomezjosem@live.com>
2024-02-27 21:29:43 +0000

    added deep equal assertion to compare different objects

--- test/add_test.js
+++ test/add_test.js
@@ -7,4 +7,17 @@ describe('+', () => {
     assert.ok(3 + 4 === 7)
 
   });
+
+  it('returns the sum of two object values', () => {
+    // Setup
+		let expected = {a: 3, b: 4, result: 7};
+		let sum = {a: 3, b: 4};
+
+    // Exercise
+		sum.result = sum.a + sum.b;
+
+    // Verify
+    assert.deepEqual(sum, expected);
+  });
+
 });
