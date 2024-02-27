668d4fc1406f54f48c3bd8645c1ad199868efd5b
Jose M. Gomez <gomezjosem@live.com>
2024-02-27 23:03:15 +0000

    test rooster object

---
+++ test/rooster_test.js
@@ -0,0 +1,66 @@
+// import modules
+const assert = require('assert')
+const Rooster = require('../rooster')
+
+describe('Rooster', () => {
+
+  describe('.announceDawn', () => {
+
+    it('returns a rooster call', () => {
+      // Setup
+      const expected = 'cock-a-doodle-doo!'
+      let actual;
+      
+      // Execute
+      actual = Rooster.announceDawn()
+
+      // Verify
+      assert.equal(expected, actual);
+
+    })
+
+  })
+
+  describe('.timeAtDawn', () => {
+    before(() => {
+      let hour;
+      let timeAtDawn;
+    })
+
+    it('returns its argument as a string', () => {
+      // Setup
+      hour = 6;
+      
+      // Execute
+      timeAtDawn = Rooster.timeAtDawn(hour);
+
+      // Verify
+      assert.strictEqual('6', timeAtDawn)
+
+    })
+
+    it('throws a range error if hour is less than 0', () => {
+      // Setup
+      hour = -1;
+
+      // Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(hour);
+      }, RangeError)
+      
+    })
+
+    it('throws an error if hour is greater than 23', () => {
+      // Setup
+      hour = 24;
+
+      // Verify
+      assert.throws(() => {
+        Rooster.timeAtDawn(hour);
+      }, RangeError)
+      
+    })
+
+  })
+
+})
